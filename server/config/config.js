0fc84eb512dd4d016c53c9eb57ee70d5ffb225b4
damith <damithasanka@gmail.com>
2017-07-13 12:16:34 +0530

    fixiing heroku issueruning

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ if(env === 'development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI =  "mongodb://localhost:27017/TodoApp";
 }
-else{
+else if(env === 'development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI =  "mongodb://localhost:27017/TodoApp-test"
 }
